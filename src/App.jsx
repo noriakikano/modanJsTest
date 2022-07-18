1f86ec7796a1327b85cdc6df1ccbcc41099f73b5
noriakikano <109402633+noriakikano@users.noreply.github.com>
2022-07-18 20:14:43 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { StrictMode } from "react";
+import ReactDom from "react-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>aaんにちは</h1>
+      <p>aaa</p>
+      <button>
+    </>
+  );
+};
+
+export default App;
